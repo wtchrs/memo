f6c11af47ae222bc4a0b98d5a2d1b191160a4aec
wtchrs <wtchr_@hotmail.com>
2026-03-09 03:06:55 +0900

    chore(frontend): update tsconfig and storybook config

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const config: StorybookConfig = {
     framework: '@storybook/react-vite',
@@ -15,6 +17,12 @@ const config: StorybookConfig = {
     ],
 
     staticDirs: ['../public'],
+
+    async viteFinal(config) {
+        return mergeConfig(config, {
+            plugins: [tsconfigPaths()],
+        })
+    }
 };
 
 export default config;
